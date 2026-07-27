6f48f4fc08c04dc3a23ca656c238eb08df9c85ab
Tobias F <ichi-ni@live.de>
2026-07-28 00:44:55 +0200

    DalaMock-Loop nicht möglich mit Dalamud 15 (native cimgui-Konflikt), Plugin-Test läuft im Spiel via Hot-Reload

--- GlamSource.Mock/Program.cs
+++ GlamSource.Mock/Program.cs
@@ -1,1 +1,20 @@
-Console.WriteLine("Hello");
+using System;
+using System.IO;
+using System.Reflection;
+using DalaMock.Core.Configuration;
+using DalaMock.Core.Plugin;
+using GlamSource;
+
+var config = new MockDalamudConfiguration
+{
+    GamePathString = @"D:\FF\game\sqpack",
+    PluginSavePathString = Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher"), "DalaMock")
+};
+
+var mockContainer = new MockContainer(dalamudConfiguration: config, askPath: false);
+
+var mockDalamudUi = mockContainer.GetMockUi();
+var pluginLoader = mockContainer.GetPluginLoader();
+var mockPlugin = pluginLoader.AddPlugin(typeof(Plugin));
+await pluginLoader.StartPlugin(mockPlugin);
+mockDalamudUi.Run();
