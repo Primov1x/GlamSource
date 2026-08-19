fd88f2493b69ca947df32a2931e0b67498b71875
Tobias F <ichi-ni@live.de>
2026-08-19 16:48:22 +0200

    chore: bump version to 0.0.0.24

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.23")]
-[assembly: AssemblyFileVersion("0.0.0.23")]
+[assembly: AssemblyVersion("0.0.0.24")]
+[assembly: AssemblyFileVersion("0.0.0.24")]
