6f1751a4bdcf84cd328b5a87dc2d8fe0f8b509ee
Tobias F <ichi-ni@live.de>
2026-08-19 02:38:16 +0200

    chore: bump version to 0.0.0.18 for gathering fix test

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.17")]
-[assembly: AssemblyFileVersion("0.0.0.17")]
+[assembly: AssemblyVersion("0.0.0.18")]
+[assembly: AssemblyFileVersion("0.0.0.18")]
