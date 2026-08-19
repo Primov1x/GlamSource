d44d1715c46c9e66b591b8f4fd9f640f44d382ed
Tobias F <ichi-ni@live.de>
2026-08-19 12:52:16 +0200

    chore: bump version to 0.0.0.20 for gather-button diagnostics

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.19")]
-[assembly: AssemblyFileVersion("0.0.0.19")]
+[assembly: AssemblyVersion("0.0.0.20")]
+[assembly: AssemblyFileVersion("0.0.0.20")]
