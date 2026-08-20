c82c505ad279b219d1e2cbaf0745e1718572d5de
Tobias F <ichi-ni@live.de>
2026-08-20 22:55:22 +0200

    fix: sync AssemblyInfo.cs version to 0.0.0.32

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.29")]
-[assembly: AssemblyFileVersion("0.0.0.29")]
+
+[assembly: AssemblyVersion("0.0.0.32")]
+[assembly: AssemblyFileVersion("0.0.0.32")]
